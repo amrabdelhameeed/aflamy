2b6baa9902a3045704ff3351d3337ee3a1a20270
Amr Abd Elhamid <theprofessional.amr@gmail.com>
2022-12-27 12:46:17 +0200

    delete unneeded onGenerateRoutes

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/cupertino.dart';
+
+// this app is small so it doesn't need this way of creating routes  
+
+/*import 'package:flutter/cupertino.dart';
 
 class AppRouter {
   Route? generateRoutes(RouteSettings settings) {
@@ -7,3 +10,4 @@ class AppRouter {
     }
   }
 }
+*/
